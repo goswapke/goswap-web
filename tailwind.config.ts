667763f0e6e52c2c9b4c53cf2090104c6bdd8fef
goswapke <philipomondi1992@gmail.com>
2025-08-28 22:37:22 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,24 +1,8 @@
-// tailwind.config.ts
 import type { Config } from "tailwindcss";
-
 const config: Config = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./styles/**/*.{css}"
-  ],
-  theme: {
-    extend: {
-      colors: {
-        "warm-gray-50": "hsl(var(--bg))",
-        navy: "hsl(var(--navy))",
-      },
-      borderColor: {
-        DEFAULT: "hsl(var(--border))",
-      },
-    },
-  },
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: { extend: {} },
   plugins: [],
 };
-
 export default config;
+
